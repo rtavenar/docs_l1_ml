5e86ed4ae6b9d4cb1059eeb89da79f6b70cabb4a
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2024-03-28 11:04:46 +0100

    styling again

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -8,7 +8,10 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "Initiation au ML"
 copyright = ""
 author = "Romain Tavenard"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", 'sphinx.ext.napoleon']
+extensions = ["sphinx.ext.autodoc", 
+              "sphinx.ext.intersphinx", 
+              'sphinx.ext.napoleon',
+              "sphinx_copybutton"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 templates_path = ["_templates"]
 html_theme = "pydata_sphinx_theme"
@@ -27,5 +30,9 @@ html_theme_options = {
 html_sidebars = {
   "*": [],
 }
+
+copybutton_exclude = '.linenos, .gp, .go'
+copybutton_copy_empty_lines = False
+
 language = "fr"
 add_module_names = False
